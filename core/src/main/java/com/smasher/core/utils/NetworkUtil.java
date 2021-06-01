09c2eaea04b136558b178f6adfefc1e809f36f49
mevermore <593982922@qq.com>
2021-06-01 18:35:26 +0800

    modify jcenter

--- core/src/main/java/com/smasher/core/utils/NetworkUtil.java
+++ core/src/main/java/com/smasher/core/utils/NetworkUtil.java
@@ -1,11 +1,20 @@
 package com.smasher.core.utils;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.net.ConnectivityManager;
+import android.net.LinkProperties;
+import android.net.Network;
+import android.net.NetworkCapabilities;
 import android.net.NetworkInfo;
+import android.net.NetworkRequest;
 import android.net.wifi.WifiInfo;
 import android.net.wifi.WifiManager;
+import android.os.Build;
 import android.telephony.TelephonyManager;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
 
 import com.smasher.core.other.ApplicationContext;
 
@@ -134,6 +143,7 @@ public class NetworkUtil {
      *
      * @return String 网络状态 {@link # NETWORKTYPE_3G}*{@link # NETWORKTYPE_INVALID},{@link # NETWORKTYPE_WAP}* <p>{@link # NETWORKTYPE_WIFI}
      */
+    @RequiresApi(api = Build.VERSION_CODES.CUPCAKE)
     public static String getNetworkType() {
         String strNetworkType = "";
         ConnectivityManager manager = (ConnectivityManager) ApplicationContext.getInstance().getSystemService(Context.CONNECTIVITY_SERVICE);
@@ -235,5 +245,112 @@ public class NetworkUtil {
                 ((ip >> 16) & 0xFF) + "." +
                 (ip >> 24 & 0xFF);
     }
+
+
+    //==================================== 分割线 =================================================
+
+
+    private static ConnectivityManager connectivityManager;
+
+
+    /**
+     * 判断当前网络是否连接
+     *
+     * @param context context
+     * @return isNetworkConnected
+     */
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    public static boolean isNetworkConnected(Context context) {
+        ConnectivityManager connectivityManager = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        Network network = connectivityManager.getActiveNetwork();
+        NetworkCapabilities networkCapabilities = connectivityManager.getNetworkCapabilities(network);
+        return networkCapabilities != null;
+    }
+
+    /**
+     * 判断当前的网络类型是wifi，流量还是有线网络
+     *
+     * @param context context
+     * @return String
+     */
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    public static String getCurrentNetworkType(Context context) {
+        ConnectivityManager connectivityManager = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        Network network = connectivityManager.getActiveNetwork();
+        NetworkCapabilities networkCapabilities = connectivityManager.getNetworkCapabilities(network);
+        if (networkCapabilities == null) {
+            return "no network";
+        } else if (networkCapabilities.hasTransport(NetworkCapabilities.TRANSPORT_WIFI)) {
+            return "Wifi";
+        } else if (networkCapabilities.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR)) {
+            return "Mobile";
+        }
+//        else if(networkCapabilities.hasCapability(NetworkCapabilities.TRANSPORT_ETHERNET)){
+//            return "Ethernet";
+//        }
+        return "other";
+    }
+
+
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    public static void registerNetworkListener(Context context) {
+        if (connectivityManager == null) {
+            connectivityManager = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        }
+
+        NetworkRequest.Builder builder = new NetworkRequest.Builder();
+        NetworkRequest request = builder.addCapability(NetworkCapabilities.NET_CAPABILITY_INTERNET)
+                .addTransportType(NetworkCapabilities.TRANSPORT_WIFI)
+                .addTransportType(NetworkCapabilities.TRANSPORT_CELLULAR)
+                .build();
+        connectivityManager.requestNetwork(request, networkCallback);
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    public static void unRegisterNetworkListener(Context context) {
+        if (connectivityManager == null) {
+            connectivityManager = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        }
+        connectivityManager.unregisterNetworkCallback(networkCallback);
+    }
+
+    @SuppressLint("newApi")
+    private static ConnectivityManager.NetworkCallback networkCallback = new ConnectivityManager.NetworkCallback() {
+        @Override
+        public void onAvailable(@NonNull Network network) {
+            super.onAvailable(network);
+        }
+
+        @Override
+        public void onLosing(@NonNull Network network, int maxMsToLive) {
+            super.onLosing(network, maxMsToLive);
+        }
+
+        @Override
+        public void onLost(@NonNull Network network) {
+            super.onLost(network);
+        }
+
+        @Override
+        public void onUnavailable() {
+            super.onUnavailable();
+        }
+
+        @Override
+        public void onCapabilitiesChanged(@NonNull Network network, @NonNull NetworkCapabilities networkCapabilities) {
+            super.onCapabilitiesChanged(network, networkCapabilities);
+        }
+
+        @Override
+        public void onLinkPropertiesChanged(@NonNull Network network, @NonNull LinkProperties linkProperties) {
+            super.onLinkPropertiesChanged(network, linkProperties);
+        }
+
+        @Override
+        public void onBlockedStatusChanged(Network network, boolean blocked) {
+            super.onBlockedStatusChanged(network, blocked);
+        }
+    };
+
 
 }
